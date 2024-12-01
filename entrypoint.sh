f46d244b5a63e902789e0ee7018ad0d65a9298b7
LamGC <lam827@lamgc.net>
2024-12-01 14:24:55 +0800

    Remove the remaining Copilot prompts and add process tracking output information.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,20 @@
 
 POWERADMIN_BASE_DIR=/var/www/html/
 
-# 在这里写一个函数，删除 install 目录
 function remove_install_dir() {
     rm -rf $POWERADMIN_BASE_DIR/install
 }
 
-# 检查 POWERADMIN_BASE_DIR/install 是否存在
+echo 'Checking install directory...'
 if [ ! -d "$POWERADMIN_BASE_DIR/install" ]; then
-    # 检查环境变量 POWERADMIN_SKIP_INSTALL 是否存在, 如果存在, 则删除整个 install 目录
-    if [ "$POWERADMIN_SKIP_INSTALL" == "true" ]; then
+    if [ "$POWERADMIN_SKIP_INSTALL" == "true" ] || [ -f "/etc/poweradmin/skip_install" ]; then
         remove_install_dir
+    else
+        echo 'Unable to find the flag to remove install, keep the install directory.'
     fi
 
-    # 否则，如果 /etc/poweradmin/skip_install 文件存在，则一样删除整个 install 目录
-    if [ -f "/etc/poweradmin/skip_install" ]; then
-        remove_install_dir
-    fi
+else
+    echo 'Cannot find the install directory, no action is required.'
 fi
 
-# 到这里，执行 php-apache 原有的 entrypoint.sh
 /usr/local/bin/docker-php-entrypoint "$@"
