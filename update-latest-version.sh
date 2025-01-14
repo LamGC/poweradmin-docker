e23ec1bf2a22c28a2fe1c2dd324d603f9562442e
LamGC <lam827@lamgc.net>
2025-01-14 09:38:56 +0800

    refactor: Use jq to retrieve the string returned by the API to ensure the correct version number is obtained.

--- update-latest-version.sh
+++ update-latest-version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-LATEST_VERSION=$(curl -s https://api.github.com/repos/poweradmin/poweradmin/releases/latest | grep tag_name | cut -d '"' -f 4)
-
-if [ -z "$LATEST_VERSION" ]; then
-  echo "Failed to fetch the latest version. Exiting."
-  exit 1
+LATEST_VERSION=$(curl -s https://api.github.com/repos/poweradmin/poweradmin/releases/latest | jq -r '.tag_name')
+CURL_EXIT_CODE=$?
+if [ $CURL_EXIT_CODE -ne 0 ] || [ -z "$LATEST_VERSION" ]; then
+    echo "Failed to retrieve the latest version"
+    exit 1
 fi
 
 if [ "$LATEST_VERSION" != "$(cat LATEST_VERSION)" ]; then
